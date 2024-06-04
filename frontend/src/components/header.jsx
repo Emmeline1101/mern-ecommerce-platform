117b3a2e27f6b160c85c6c675fb7a481e841d40d
Emmeline1101 <emmelinexu23@gmail.com>
2024-06-03 18:02:33 -0700

    update header.jsx

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,16 @@
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+const header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default header
