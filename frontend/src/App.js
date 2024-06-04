01419b012d0e58cd8b1a7cb73a45baeb5d7d434f
Emmeline1101 <emmelinexu23@gmail.com>
2024-06-03 18:41:11 -0700

    update app.js and header.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './components/header';
+import { Container } from "react-bootstrap";
 
+ 
 const App = () => {
   return (
-    <h1>Welcome to Emmeline's Shop</h1>
+    <>
+      <main className='py-3'>
+        <Container>
+        <h1>Welcome to Emmeline's Shop</h1>
+        </Container>
+      </main>
+     
+    </>
+   
   )
 }
 
