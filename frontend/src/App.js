2da846e670c815672a08d85a0f17ec76d94d0f6f
Emmeline1101 <emmelinexu23@gmail.com>
2024-06-30 23:35:10 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <h1>Welcome to Emmeline's Shop</h1>
-          <HomeScreen/ >
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
