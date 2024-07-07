4f5d753aaa360e4fa4a17a66597240f1b2ae85d7
Emmeline1101 <emmelinexu23@gmail.com>
2024-07-06 17:19:04 -0700

    setup node backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import products from './data/products.js'; // do not forhet to add the .js extension in ES
+const port = 5001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+    });
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+}); 
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id); // if the product id matches the id in the url, then we will return that product
+    res.json(product);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
