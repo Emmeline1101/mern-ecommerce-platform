94b1a1ee9aac91d5acd624a71435d9fc9d851b93
Emmeline1101 <emmelinexu23@gmail.com>
2024-07-06 20:07:44 -0700

    fetch product data with Axios

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
 import Product from '../components/Product';
+import axios from 'axios';
 
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]); // we are using useState to set the products to an empty array
+   useEffect(() => {
+    const fetchProducts = async () => {
+      // we are fetching the products from the backend, since we added the proxy in the package.json file, we do not need to add the http://localhost:5001  
+      const {data} = await axios.get('/api/products'); 
+      setProducts(data); // we are setting the products
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
           <h1>Latest Products</h1>
