67a8dccf89ad3812afe2104bbcdc603d15d45e2c
Emmeline1101 <emmelinexu23@gmail.com>
2024-07-15 19:53:31 -0700

    Add Spinner  & Loader

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,8 @@ import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 // import axios from 'axios';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 
 const HomeScreen = () => {
@@ -20,12 +22,12 @@ const HomeScreen = () => {
   return (
     <>
           { isLoading ? (
-            <h2>Loading...</h2>
-          ) : error ? (<div>{error?.data?.message || error.error}</div>) : 
+            <Loader />
+          ) : error ? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) : 
           
           (<>
                     <h1>Latest Products</h1>
-                    <Row>
+                    <Row> 
                       {products.map((product) => ( 
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                           <Product product={product} />
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { useParams, Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice'; 
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const ProductScreen = () => {
     const { id: productId } = useParams();  // get id from url by params
@@ -13,9 +15,9 @@ const ProductScreen = () => {
             <Link className='btn btn-light my-3' to="/">Go Back</Link>
             
             {isLoading ?(
-                <h2>Loading...</h2>
+                <Loader />
             ) : error ? (
-                <div>{error?.data?.message || error.toString()}</div>
+                <Message variant='danger'>{error?.data?.message || error.toString()}</Message>
             ) : (
                 <Row>
                 <Col md={5}>
